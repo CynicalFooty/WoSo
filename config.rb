f894474f7cc80bf3d1d516c0186bcaf05667e8ef
Danny Page <daniel.w.page@gmail.com>
2015-08-11 09:14:45 -0400

    Fixes to relative links

--- config.rb
+++ config.rb
@@ -5,11 +5,13 @@ require 'tasks'
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 
 #options
-set :http_prefix, "/WoSo"
+activate :relative_assets
+set :relative_links, true
 activate :automatic_image_sizes
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
 DB = Utils.open_db
 
 #Setup Info
@@ -24,13 +26,13 @@ build_hash[:teams].each do |team|
   players = roster.map { |r| Players.find(id: r[:player_id]) }
   proxy "/teams/#{team[:alias].downcase}/index.html", "/teams/single.html", :locals => { :team_info => team, :roster => players }, :ignore => true
 end
-proxy 'teams/index.html', '/teams/list.html', :locals => { :teams => build_hash[:teams]}, :ignore => true
+proxy '/teams/index.html', '/teams/list.html', :locals => { :teams => build_hash[:teams]}, :ignore => true
 
 #page "players/*", :layout => :players
 build_hash[:players].each do |player|
   proxy "/players/#{player[:full_name].gsub(/[^0-9a-z]/i, '').downcase}/index.html", "/players/single.html", :locals => { :player => player }, :ignore => true
 end
-proxy 'players/index.html', '/players/list.html', :locals => { :players => build_hash[:players]}, :ignore => true
+proxy '/players/index.html', '/players/list.html', :locals => { :players => build_hash[:players]}, :ignore => true
 
 activate :deploy do |deploy|
   deploy.build_before = true # default: false
